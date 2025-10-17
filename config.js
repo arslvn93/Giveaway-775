1ba97dd9e26a049af2626eaeb88ebc61fd700625
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-10-17 15:01:14 -0400

    Update Site

--- config.js
+++ config.js
@@ -93,7 +93,7 @@ const config = {
       {
         "icon": "fa-brands fa-square-instagram",
         "title": "Follow My Instagram",
-        "description": "follow @@archetyperealtygroup\n\n"
+        "description": "follow @archetyperealtygroup\n\n"
       },
       {
         "icon": "fas fa-edit",
@@ -188,7 +188,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1760727476038_0",
+      "id": "q_auto_1760727673820_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -210,7 +210,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760727476038_1",
+      "id": "q_auto_1760727673820_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -232,7 +232,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760727476038_2",
+      "id": "q_auto_1760727673820_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {
